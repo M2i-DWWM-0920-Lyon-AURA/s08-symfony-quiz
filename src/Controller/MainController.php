7673239ca190aeb8075e26267d80be2e7e3ccb6e
Romain Tello <romain.tello@gmail.com>
2020-11-04 10:16:19 +0100

    Add create page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -18,6 +19,25 @@ class MainController extends AbstractController
         // Renvoie une vue affichant la page d'accueil
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/create", name="create")
+     */
+    public function create(): Response
+    {
+        // Récupère l'utilisateur actuellement connecté
+        $currentUser = $this->getUser();
+
+        // Récupère la liste des quiz publiés par l'utilisateur connecté
+        $quizzes = $currentUser->getPlayer()->getQuizzes();
+
+        // Renvoie une vue affichant la liste des quiz créés par l'utilisateur actuellement connecté
+        // en vue de les modifier
+        return $this->render('main/create.html.twig', [
+            'quizzes' => $quizzes
         ]);
     }
 }
